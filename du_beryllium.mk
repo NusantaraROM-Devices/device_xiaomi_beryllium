e7a5a0784707070717b6a736b950a7b69d5ea96e
goodmeow <harunbam3@gmail.com>
2020-04-03 05:15:26 +0700

    beryllium: inherit firmware & remove modem assertion

---
+++ du_beryllium.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/beryllium/device.mk)
+$(call inherit-product, vendor/xiaomi/firmware/beryllium/firmware.mk)
+
+# Inherit some common CORVUS-OS stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_INCLUDE_WIFI_EXT := true
+DU_BUILD_TYPE := OFFICIAL
+# CURRENT_BUILD_TYPE :=gapps
+# TARGET_GAPPS_ARCH :=arm64
+#$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := du_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO F1
+PRODUCT_MANUFACTURER := Xiaomi
+
+
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.6.0.QEJMIXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.6.0.QEJMIXM release-keys" \
+    PRODUCT_NAME="beryllium" \
+    TARGET_DEVICE="beryllium"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
