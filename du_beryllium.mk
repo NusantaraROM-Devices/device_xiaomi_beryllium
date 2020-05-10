07fc5915f51c7f6bf47be2a49f13724f3e502f2e
goodmeow <harunbam3@gmail.com>
2020-05-10 16:50:05 +0700

    beryllium: update dep. #2
    - Device Coral May FP

--- du_beryllium.mk
+++ du_beryllium.mk
@@ -12,9 +12,7 @@ $(call inherit-product, vendor/du/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_INCLUDE_WIFI_EXT := true
 DU_BUILD_TYPE := GaijinShogun
-# CURRENT_BUILD_TYPE :=gapps
-# TARGET_GAPPS_ARCH :=arm64
-#$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := du_beryllium
@@ -24,7 +22,7 @@ PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.6.0.QEJMIXM release-keys" \
