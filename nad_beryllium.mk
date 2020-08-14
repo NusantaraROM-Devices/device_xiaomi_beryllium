ee7c6f0a0140609e34c1dde84b021a12d3a31142
goodmeow <harunbam3@gmail.com>
2020-08-14 11:00:31 +0700

    beryllium: build pixel offline charging

--- nad_beryllium.mk
+++ nad_beryllium.mk
@@ -12,6 +12,7 @@ $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_WIFI_EXT := true
 NAD_BUILD_TYPE := OFFICIAL
+USE_PIXEL_CHARGING := true
 
 
 # Device identifier. This must come after all inclusions.
