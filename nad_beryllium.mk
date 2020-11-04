8630ec49f46306e96f631e4517b5e5d3939140fe
goodmeow <harunbam3@gmail.com>
2020-11-04 22:30:04 +0700

    beryllium: update device Fingerprint from Coral RP1A.201105.002 also update private build desc

--- nad_beryllium.mk
+++ nad_beryllium.mk
@@ -18,10 +18,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Pocophone F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.1.0 QEJMIXM release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.2.0 QEJMIXM release-keys" \
     PRODUCT_NAME="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
