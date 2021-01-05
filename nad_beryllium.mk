a6a765d8e5e5a0c92f5843d4486faf3e41e81147
goodmeow <harunbam3@gmail.com>
2021-01-05 09:12:04 +0700

    beryllium: update device FP from redfin RQ1A.210105.003

--- nad_beryllium.mk
+++ nad_beryllium.mk
@@ -18,7 +18,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Pocophone F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.201205.011/6966805:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.2.0 QEJMIXM release-keys" \
