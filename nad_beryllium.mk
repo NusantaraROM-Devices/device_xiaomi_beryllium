4f5e30b898b3ec5acd9a514c755e0a72f22782cf
zkyzzz <muhadlim17@gmail.com>
2022-04-19 18:49:28 +0700

    beryllium: Switch back to Stock FP
    Signed-off-by: zkyzzz <zkyzzz@nusantararom.org>

--- nad_beryllium.mk
+++ nad_beryllium.mk
@@ -20,10 +20,10 @@ PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Pocophone F1
 PRODUCT_MANUFACTURER := Xiaomi
-BUILD_FINGERPRINT := "google/raven/raven:12/S3B1.220218.004/8242181:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 S3B1.220218.004 8242181 release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
     PRODUCT_NAME="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
