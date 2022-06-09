f37897e27fff8d7a8d10f65960582b34f6988876
zkyzzz <zkyzzz@nusantararom.org>
2022-06-09 18:26:05 +0800

    beryllium: VoidUI Bringup
    Signed-off-by: zkyzzz <zkyzzz@nusantararom.org>

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -6,11 +6,22 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common VoidUI stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+IS_PHONE := true
+
+# VoidUI Maintainer Flags
+VOID_MAINTAINER := A. Dzaky
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
