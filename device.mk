a39035edbd434510f1bb72848873100c4a1d899f
zkyzzz <zkyzzz@nusantararom.org>
2022-06-09 18:26:05 +0800

    beryllium: Nusantara-ify
    Signed-off-by: zkyzzz <zkyzzz@nusantararom.org>

--- device.mk
+++ device.mk
@@ -27,6 +27,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-nad \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
--- nad_beryllium.mk
+++ nad_beryllium.mk
@@ -6,11 +6,16 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Nusantara stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+
+NAD_BUILD_TYPE := OFFICIAL
+#USE_GAPPS := true
+USE_AOSP_CLOCK := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := nad_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
